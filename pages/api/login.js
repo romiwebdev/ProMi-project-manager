f27f3ff0cf4c8eb30d769ffc9d1e5800b3b65dee
rominmuh230 <rominmuh230@gmail.com>
2025-06-15 00:55:01 +0000

    update error login

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,17 +1,17 @@
-// pages/api/login.js (contoh nama file)
+// pages/api/login.js
 export default function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
   const { username, password } = req.body;
 
-  // Ambil dari .env.local
+  // Ambil dari .env.local (Next.js otomatis membacanya)
   const validUsername = process.env.ADMIN_USERNAME;
   const validPassword = process.env.ADMIN_PASSWORD;
 
   // Validasi
   if (username === validUsername && password === validPassword) {
-      res.status(200).json({ success: true });
+    res.status(200).json({ success: true });
   } else {
-      res.status(401).json({ success: false, message: 'Login gagal' });
+    res.status(401).json({ success: false, message: 'Login gagal' });
   }
 }
